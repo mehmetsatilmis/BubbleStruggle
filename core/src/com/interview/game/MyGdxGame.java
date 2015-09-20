137a14b7e845b2e124aa3cc8d0da8d3932f366c4
mehmetsatilmis <mehmetsatilmisgyte@gmail.com>
2015-09-20 17:08:05 +0300

    -bug fix

--- core/src/com/interview/game/MyGdxGame.java
+++ core/src/com/interview/game/MyGdxGame.java
@@ -2,22 +2,30 @@ package com.interview.game;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.interview.game.Model.InputManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.interview.game.Manager.FileManager;
+import com.interview.game.Manager.InputManager;
 import com.interview.game.State.PlayState;
 
 public class MyGdxGame implements ApplicationListener {
     private PlayState playState;
     private InputManager inputManager;
+    private SpriteBatch spriteBatch;
+    private Texture  backgroundImage;
 
     public static final float STEP = 1 / 60f;
     private float accum;
 
-	@Override
-	public void create () {
+    @Override
+    public void create () {
 
         playState = new PlayState();
         inputManager = new InputManager();
-	}
+
+        spriteBatch = new SpriteBatch();
+        backgroundImage = FileManager.getManager().getTexture(FileManager.BACKGROUND_IMAGE,"background");
+    }
 
     @Override
     public void resize(int width, int height) {
@@ -25,15 +33,15 @@ public class MyGdxGame implements ApplicationListener {
     }
 
     @Override
-	public void render () {
+    public void render () {
         accum += Gdx.graphics.getDeltaTime();
         while(accum >= STEP) {
             accum -= STEP;
             inputManager.update();
             playState.update(STEP);
-            playState.render();
+            playState.render(spriteBatch);
         }
-	}
+    }
 
     @Override
     public void pause() {
